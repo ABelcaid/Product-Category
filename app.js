73fc2892160d01a2f1ff8666e3d718baa3a411b4
Idoufkir <mustafa.idoufkir@gmail.com>
2021-01-06 10:36:05 +0100

    i

--- app.js
+++ app.js
@@ -30,11 +30,67 @@ app.set('views', __dirname + '/views'); // set express to look in this folder to
 app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
+<<<<<<< Updated upstream
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+=======
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','css')));
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','img')));
+app.use(express.static(path.join(__dirname, 'public', 'dashboard','lib')));
+app.use(express.static(publicDirectory));
+
+
+//------------------------------------ Routes Parts ----------------------------------//
+
+//------------------------------------ (HomeRoute) ----------------------------------//
+
+
+
+app.get('/', (req, res) => {
+  const sql = "SELECT * FROM product INNER JOIN category WHERE product.category = category.idc";
+     db.query(sql, (err, rows) => {
+      console.log(rows);
+         if (err) throw err;
+         res.render('index', {
+          title : 'E-com Relation',
+             rows: rows
+         });
+ 
+     })
+ 
+ });
+
+//------------------------------------ (Product) ----------------------------------//
+
+app.get('/add-product',(req, res) => {
+  let sql = "SELECT * FROM category";
+  db.query(sql, (err, rows) => {
+      if(err) throw err;
+      res.render('addProduct', {
+          title : 'Add Product',
+          rows : rows
+      });
+
+  });
+  });
+
+
+//------------------------------------ (Categorie) ----------------------------------//
+
+app.get('/add-category',(req, res) => {
+
+  res.render('addCategory', {
+      title : 'Add Product',
+
+  });
+
+});
+
+>>>>>>> Stashed changes
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
