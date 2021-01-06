1c80e54c2cd0691c4133e5be610d10e5767ed2a5
Idoufkir <mustafa.idoufkir@gmail.com>
2021-01-06 10:42:31 +0100

    Update DB

--- SQL/product-category.sql
+++ SQL/product-category.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 04, 2021 at 11:20 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 06 jan. 2021 à 10:42
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,75 +18,93 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `product-category`
+-- Base de données : `product-category`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Structure de la table `category`
 --
 
 CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
+  `idc` int(11) NOT NULL,
+  `name_c` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`idc`, `name_c`) VALUES
+(1, 'accessories'),
+(2, 'Mobile'),
+(3, 'Computer');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Structure de la table `product`
 --
 
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
+  `idp` int(11) NOT NULL,
+  `name_p` varchar(200) NOT NULL,
   `price` int(11) NOT NULL,
   `category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`idp`, `name_p`, `price`, `category`) VALUES
+(1, 'Product 1', 199, 1),
+(2, 'Product 2', 299, 1),
+(3, 'Product 3', 2999, 3);
+
+--
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `category`
+-- Index pour la table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`idc`);
 
 --
--- Indexes for table `product`
+-- Index pour la table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`idp`),
   ADD KEY `FK_PersonOrder` (`category`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `product`
+-- Contraintes pour la table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `FK_PersonOrder` FOREIGN KEY (`category`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `FK_PersonOrder` FOREIGN KEY (`category`) REFERENCES `category` (`idc`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
